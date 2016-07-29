39b75c5747be8bd8aba5dccb3ee075cbb366c9ce
duytri <duytri@PTNHTTT07>
2016-07-29 17:09:14 +0700

    Add HELP

--- TFIDFStat/src/main/scala/tfidfStat.scala
+++ TFIDFStat/src/main/scala/tfidfStat.scala
@@ -14,89 +14,93 @@ object tfidfStat {
    * param 2: ten thu muc ben trong thu muc input (la 0 hay 1).
    * */
   def main(args: Array[String]): Unit = {
-    args.foreach { println }
-    val inputPath = args(0) + "input" + File.separator + args(2)
-    val outputPath = args(1)
-    val inputFiles = (new File(inputPath)).listFiles()
-    val outputFile = outputPath + File.separator + "result.csv"
+    if (args(0).equalsIgnoreCase("--help")) {
+      println("Cac tham so cua chuong trinh:\n\t* param 0: duong dan den thu muc input va libs (chua file thu vien stopword). CO splash o cuoi.\n\t* param 1: duong dan thu muc xuat file ket qua.\n\t* param 2: ten thu muc ben trong thu muc input (la 0 hay 1).")
+    } else {
+      args.foreach { println }
+      val inputPath = args(0) + "input" + File.separator + args(2)
+      val outputPath = args(1)
+      val inputFiles = (new File(inputPath)).listFiles()
+      val outputFile = outputPath + File.separator + "result.csv"
 
-    println("Input path: " + inputPath)
-    println("Output path: " + outputFile)
-    println("Start analyze ...")
+      println("Input path: " + inputPath)
+      println("Output path: " + outputFile)
+      println("Start analyze ...")
 
-    //~~~~~~~~~~ Get input files ~~~~~~~~~~
-    var wordSetByFile = new ArrayBuffer[Map[String, Int]](inputFiles.length) // Map[word, frequency in document]
-    // Foreach text file
-    for (i <- 0 to inputFiles.length - 1) {
-      var wordsTmpArr = new ArrayBuffer[String]
-      val source = Source.fromFile(inputFiles(i).getAbsolutePath, "utf-8")
-      source.getLines.foreach { x => wordsTmpArr.append(x) }
-      // Fixed too many open files exception
-      source.close
-      wordSetByFile.append(addOrIgnore(wordsTmpArr))
-    }
+      //~~~~~~~~~~ Get input files ~~~~~~~~~~
+      var wordSetByFile = new ArrayBuffer[Map[String, Int]](inputFiles.length) // Map[word, frequency in document]
+      // Foreach text file
+      for (i <- 0 to inputFiles.length - 1) {
+        var wordsTmpArr = new ArrayBuffer[String]
+        val source = Source.fromFile(inputFiles(i).getAbsolutePath, "utf-8")
+        source.getLines.foreach { x => wordsTmpArr.append(x) }
+        // Fixed too many open files exception
+        source.close
+        wordSetByFile.append(addOrIgnore(wordsTmpArr))
+      }
 
-    var arrStopwords = new ArrayBuffer[String]
-    val stopwordFilePath = args(0) + "libs/vietnamese-stopwords.txt"
-    val swSource = Source.fromFile(stopwordFilePath)
-    swSource.getLines.foreach { x => arrStopwords.append(x) }
-    swSource.close
+      var arrStopwords = new ArrayBuffer[String]
+      val stopwordFilePath = args(0) + "libs/vietnamese-stopwords.txt"
+      val swSource = Source.fromFile(stopwordFilePath)
+      swSource.getLines.foreach { x => arrStopwords.append(x) }
+      swSource.close
 
-    wordSetByFile.foreach(oneSet => oneSet --= arrStopwords)
+      wordSetByFile.foreach(oneSet => oneSet --= arrStopwords)
 
-    //~~~~~~~~~~ Calculate TFIDF ~~~~~~~~~~
-    var tfidfWordSet = new ArrayBuffer[Map[String, Double]](inputFiles.length) // Map[word, TF*IDF-value]
+      //~~~~~~~~~~ Calculate TFIDF ~~~~~~~~~~
+      var tfidfWordSet = new ArrayBuffer[Map[String, Double]](inputFiles.length) // Map[word, TF*IDF-value]
 
-    for (i <- 0 to inputFiles.length - 1) {
-      var tfidfOneDoc = Map[String, Double]()
-      for (oneWord <- wordSetByFile(i)) {
-        if (oneWord._1.equals("theverge")) {
-          var wordCount = 0d
-          wordSetByFile(i).foreach((x => wordCount += x._2))
-          println("Freq = " + oneWord._2 + " WordNo. = " + wordCount + " TF = " + oneWord._2 / wordCount)
+      for (i <- 0 to inputFiles.length - 1) {
+        var tfidfOneDoc = Map[String, Double]()
+        for (oneWord <- wordSetByFile(i)) {
+          if (oneWord._1.equals("theverge")) {
+            var wordCount = 0d
+            wordSetByFile(i).foreach((x => wordCount += x._2))
+            println("Freq = " + oneWord._2 + " WordNo. = " + wordCount + " TF = " + oneWord._2 / wordCount)
 
-          var n = 0d
-          wordSetByFile.foreach(doc => {
-            if (doc.contains(oneWord._1)) n += 1
-          })
+            var n = 0d
+            wordSetByFile.foreach(doc => {
+              if (doc.contains(oneWord._1)) n += 1
+            })
 
-          println("DocNo. = " + wordSetByFile.length + " DocContainsNo. = " + n + " IDF = " + Math.log10(wordSetByFile.length / n))
+            println("DocNo. = " + wordSetByFile.length + " DocContainsNo. = " + n + " IDF = " + Math.log10(wordSetByFile.length / n))
 
-          wordSetByFile(i).foreach(x => println(x._1 + " ++++ " + x._2))
+            //wordSetByFile(i).foreach(x => println(x._1 + " ++++ " + x._2))
 
+          }
+          tfidfOneDoc += oneWord._1 -> TFIDFCalc.tfIdf(oneWord._1, i, wordSetByFile)
         }
-        tfidfOneDoc += oneWord._1 -> TFIDFCalc.tfIdf(oneWord._1, i, wordSetByFile)
+        tfidfWordSet.append(tfidfOneDoc)
       }
-      tfidfWordSet.append(tfidfOneDoc)
-    }
 
-    //~~~~~~~~~~ Write to files ~~~~~~~~~~~
-    for (i <- 0 to inputFiles.length - 1) {
-      write2File(tfidfWordSet(i), outputFile)
-    }
-
-    println("Finished!!!")
-  }
-
-  def addOrIgnore(someWords: ArrayBuffer[String]): Map[String, Int] = {
-    var eachWordSet = Map[String, Int]()
-    someWords.foreach { x =>
-      {
-        if (!eachWordSet.contains(x))
-          eachWordSet += (x -> 1)
-        else eachWordSet.update(x, eachWordSet(x) + 1)
+      //~~~~~~~~~~ Write to files ~~~~~~~~~~~
+      for (i <- 0 to inputFiles.length - 1) {
+        write2File(tfidfWordSet(i), outputFile)
       }
-    }
-    eachWordSet
-  }
 
-  def write2File(array: Map[String, Double], filePath: String): Unit = {
-    val file = new File(filePath)
-    val bw = new BufferedWriter(new FileWriter(file, true))
-    bw.flush()
-    array.foreach { x =>
-      bw.write(x._1 + ";" + x._2 + "\n")
+      println("Finished!!!")
     }
-    bw.close()
+
+    def addOrIgnore(someWords: ArrayBuffer[String]): Map[String, Int] = {
+      var eachWordSet = Map[String, Int]()
+      someWords.foreach { x =>
+        {
+          if (!eachWordSet.contains(x))
+            eachWordSet += (x -> 1)
+          else eachWordSet.update(x, eachWordSet(x) + 1)
+        }
+      }
+      eachWordSet
+    }
+
+    def write2File(array: Map[String, Double], filePath: String): Unit = {
+      val file = new File(filePath)
+      val bw = new BufferedWriter(new FileWriter(file, true))
+      bw.flush()
+      array.foreach { x =>
+        bw.write(x._1 + "|" + x._2 + "\n")
+      }
+      bw.close()
+    }
   }
 }
